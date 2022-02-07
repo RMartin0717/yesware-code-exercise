37926b168508635aac3a1bb87b615ecd2590623e
Riley Martin <riley.willow@gmail.com>
2022-02-06 18:49:02 -0700

    Structure name data to be an array of objects with key-value pairs for first and last names, alphabetized by last name

--- actor-name-program.js
+++ actor-name-program.js
@@ -1,5 +1,65 @@
 const fs = require('fs')
+const fileContents = fs.readFileSync('./testData.txt').toString()
+// const fileContents = fs.readFileSync('./data.txt').toString()
 
-const fileContents = fs.readFileSync('./data.txt').toString()
 
-console.log(typeof fileContents)
+class ActorNameProgram {
+  constructor(nameData) {
+    this.nameData = this.structureData(nameData)
+    // this.nameData = (typeof fileContents)
+  }
+
+  structureData(data) {
+    const separatedNames = data.split('.\n')
+    const orderedNames = separatedNames.sort()
+    //consider using forEach instead of reduce?
+    const structuredNameData = orderedNames.reduce((acc, name) => {
+      const splitLastName = name.split(', ')
+      if(!splitLastName[1]) {
+        return acc
+      } else {
+        const lastName = splitLastName[0]
+        const splitFirstName = splitLastName[1].split(' --')
+        const firstName = splitFirstName[0]
+        const newName = { lastName: lastName, firstName: firstName }
+        return [...acc, newName]
+      }
+    }, [])
+    return structuredNameData
+  }
+
+  uniqueFullNameCount() {
+
+  }
+
+  uniqueLastNameCount() {
+
+  }
+
+  uniqueFirstNameCount() {
+
+  }
+
+  mostCommonLastNames() {
+
+  }
+
+  mostCommonFirstNames() {
+
+  }
+
+  speciallyUniqueNames() {
+
+  }
+
+  modifiedNames() {
+
+  }
+
+  callAllOutptMethods() {
+
+  }
+}
+
+const names = new ActorNameProgram(fileContents)
+console.log(names.nameData)
