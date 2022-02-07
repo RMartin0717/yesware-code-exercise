60f533ffca809cb146c7de5bf82597ca09a45111
Riley Martin <riley.willow@gmail.com>
2022-02-06 19:13:04 -0700

    Add method to check name validity before adding it to the array of name objects

--- actor-name-program.js
+++ actor-name-program.js
@@ -6,26 +6,38 @@ const fileContents = fs.readFileSync('./testData.txt').toString()
 class ActorNameProgram {
   constructor(nameData) {
     this.nameData = this.structureData(nameData)
-    // this.nameData = (typeof fileContents)
   }
 
   structureData(data) {
     const separatedNames = data.split('.\n')
     const orderedNames = separatedNames.sort()
     //consider using forEach instead of reduce?
+    //maybe in favor of reduce for immutability
     const structuredNameData = orderedNames.reduce((acc, name) => {
       const splitLastName = name.split(', ')
+      //last element in separatedNames array comes back as empty string because it splits at the period/line break and sometimes theres an empty line after
       if(!splitLastName[1]) {
         return acc
       } else {
         const lastName = splitLastName[0]
         const splitFirstName = splitLastName[1].split(' --')
         const firstName = splitFirstName[0]
+        if(this.checkNameNotValid(lastName) || this.checkNameNotValid(firstName)) {
+          return acc
+        }
         const newName = { lastName: lastName, firstName: firstName }
         return [...acc, newName]
       }
     }, [])
     return structuredNameData
+  }
+
+  checkNameNotValid(name) {
+    const splitName = name.toUpperCase().split('')
+    const notValid = splitName.find(value => value.charCodeAt(0) < 65 || value.charCodeAt(0) > 90)
+    if(notValid) {
+      return true
+    }
   }
 
   uniqueFullNameCount() {
